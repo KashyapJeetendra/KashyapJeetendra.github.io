321264c7fe581814dec17ead6e341810187a6b24
Jeetendra Kashyap <jkashyap@netlink.com>
2021-09-06 12:51:09 +0530

    Fixed sitemap

--- _code/page_gen.py
+++ _code/page_gen.py
@@ -9,8 +9,7 @@ highlight_js = get(
     ).content.decode()
 
 
-xml = """
-<?xml version="1.0" encoding="UTF-8"?>
+xml = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 {}
 </urlset>
